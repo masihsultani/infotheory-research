abd789eb4c088c878816ad28ed177af9ea83b603
Masih <masih@cs.toronto.edu>
2019-09-17 20:40:27 -0400

    fix surprisal

--- compute_all.py
+++ compute_all.py
@@ -5,18 +5,15 @@ from ngram_cosine import compute_cosine
 from ngram_surprisal import compute_surprisal
 from helper import get_gram_count
 import gensim
+import pandas as pd
 
 from gensim.scripts.glove2word2vec import glove2word2vec
-from gensim.test.utils import datapath, get_tmpfile
-
-
-
-
+from gensim.test.utils import get_tmpfile
 
 if __name__ == "__main__":
     corpus = sys.argv[1]  # native or nonnative or google or wiki
     stop_word = sys.argv[2]
-    ngrams =  sys.argv[3]
+    ngrams = sys.argv[3]
     m = sys.argv[4]
     if m == "glove":
         glove_file = '/hal9000/masih/models/glove.6B.300d.txt'
@@ -25,27 +22,30 @@ if __name__ == "__main__":
         glove2word2vec(glove_file, tmp_file)
         model = gensim.models.KeyedVectors.load_word2vec_format(tmp_file)
     else:
-        model = gensim.models.KeyedVectors.load_word2vec_format('/hal9000/masih/models/GoogleNews-vectors-negative300.bin',
-                                                                                                  binary=True)
+        model = gensim.models.KeyedVectors.load_word2vec_format(
+            '/hal9000/masih/models/GoogleNews-vectors-negative300.bin',
+            binary=True)
 
     if corpus == 'google':
         filein = f"/w/nobackup/131/web1t-5gram-v1"
         stop_word = None
     else:
         filein = f"/ais/hal9000/masih/surprisal/{corpus}/"
+    df = pd.read_csv("all_forms.csv", encoding="utf-8")  # load csv with long and short form words
+    short_forms = set(list(df.short.values))
 
-    entropy_dict = compute_entropy(filein, corpus,ngrams,stop_word)
-    cosine_dict = compute_cosine(filein, corpus, ngrams, model,stop_word)
-    surprisal_list = compute_surprisal(filein, corpus, ngrams,stop_word)
-    unigram_count = get_gram_count("unigram",filein,corpus,stop_word)
+    entropy_dict = compute_entropy(filein, corpus, ngrams, stop_word)
+    cosine_dict = compute_cosine(filein, corpus, ngrams, model, stop_word)
+    surprisal_dict = compute_surprisal(filein, corpus, ngrams, stop_word)
+    unigram_count = get_gram_count("unigram", filein, corpus, stop_word)
     sense_dict = {'porn': 1, 'photo': 2, 'phone': 3, 'bike': 4, 'tv': 5, 'carb': 6, 'math': 7, 'limo': 8,
-                 'ref': 15, 'roach': 16, 'fridge': 17, 'exam': 18, 'chemo': 19, 'sax': 20, 'frat': 21, 'memo': 22,
-                 'dorm': 9, 'kilo': 10, 'rhino': 11, 'undergrad': 12, 'hippo': 13, 'chimp': 14,
-                 'telephone': 3, 'refrigerator': 17, 'undergraduate': 12, 'mathematics': 7,
-                 'examination': 18, 'television': 5, 'photograph': 2, 'memorandum': 22, 'bicycle': 4,
-                 'pornography': 1, 'fraternity': 21, 'limousine': 8, 'referee': 15, 'saxophone': 20,
-                 'carbohydrate': 6, 'chemotherapy': 19, 'hippopotamus': 13, 'cockroach': 16,
-                 'kilogram': 10, 'rhinoceros': 11, 'dormitory': 9, 'chimpanzee': 14}
+                  'ref': 15, 'roach': 16, 'fridge': 17, 'exam': 18, 'chemo': 19, 'sax': 20, 'frat': 21, 'memo': 22,
+                  'dorm': 9, 'kilo': 10, 'rhino': 11, 'undergrad': 12, 'hippo': 13, 'chimp': 14,
+                  'telephone': 3, 'refrigerator': 17, 'undergraduate': 12, 'mathematics': 7,
+                  'examination': 18, 'television': 5, 'photograph': 2, 'memorandum': 22, 'bicycle': 4,
+                  'pornography': 1, 'fraternity': 21, 'limousine': 8, 'referee': 15, 'saxophone': 20,
+                  'carbohydrate': 6, 'chemotherapy': 19, 'hippopotamus': 13, 'cockroach': 16,
+                  'kilogram': 10, 'rhinoceros': 11, 'dormitory': 9, 'chimpanzee': 14}
 
     if corpus == "google":
         out_file = f"/hal9000/masih/surprisal/all_data/{corpus}_{ngrams}.csv"
@@ -54,11 +54,17 @@ if __name__ == "__main__":
 
     with open(out_file, 'w') as out_put:
         csvout = csv.writer(out_put)
-        header =["context","word","form", "sense","count", "surprisal","entropy","cosine"]
+        header = ["context", "word", "form", "sense", "count", "surprisal", "entropy", "cosine"]
         csvout.writerow(header)
-        for row in surprisal_list:
-            word = row[1]
-            context = row[0]
-            final_row = row[:-1] + [sense_dict[word]] +[unigram_count[word]] +[row[-1]] +[entropy_dict[context]] + [cosine_dict[context][word]]
-            csvout.writerow(final_row)
+
+        for word in surprisal_dict:
+            if word in short_forms:
+                i = 0
+            else:
+                i = 1
+            for context in surprisal_dict[word]:
+                surprisal = surprisal_dict[word][context]
+                final_row = [context, word, i, sense_dict[word], unigram_count[word], surprisal, entropy_dict[context],
+                             cosine_dict[context][word]]
+                csvout.writerow(final_row)
     out_put.close()
--- ngram_surprisal.py
+++ ngram_surprisal.py
@@ -16,21 +16,13 @@ def compute_surprisal(infolder, corpus, ngrams, stop_word):
     short_forms = set(list(df.short.values))
     long_forms = set(list(df.long.values))
     all_forms = short_forms | long_forms
-    surprisal_lst = []
-    context_count = get_gram_count(gram_conv[ngrams], infolder,corpus, stop_word)
-    probs = get_ngram_probs(ngrams,context_count,all_forms,infolder,corpus,stop_word)
+
+    context_count = get_gram_count(gram_conv[ngrams], infolder, corpus, stop_word)
+    probs = get_ngram_probs(ngrams, context_count, all_forms, infolder, corpus, stop_word)
     del context_count
     for word in probs:
         for context in probs[word]:
-            surprisal = np.log2(1/probs[word][context])
-            if word in short_forms:
-                temp_lst = [context,word,0,surprisal]
-            else:
-                temp_lst = [context, word, 1, surprisal]
-            surprisal_lst.append(temp_lst)
-            del probs[word][context]
+            p = probs[word][context]
+            probs[word][context] = np.log2(1 / p)
 
-
-
-
-    return surprisal_lst
+    return probs

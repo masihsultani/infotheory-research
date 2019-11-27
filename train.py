3370d756be1f8b21911fbc4e5185efe1d78b7342
Masih <masih@cs.toronto.edu>
2019-11-27 15:01:40 -0500

    now allows for Random Forest Classifier

--- train.py
+++ train.py
@@ -1,29 +1,27 @@
 import pandas as pd
 import numpy as np
+import sys
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.model_selection import cross_val_score
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.linear_model import LogisticRegression
 
 
-def train_model(df):
+def train_model(df, model):
     """
-    A function that converts text data into M x N matrix where M
-    is number of headlines, and N is feature space dimension(vocabulary)
+    A function that converts text data into N x M matrix where N
+    is number of data points/sentences, and N is feature space dimension(vocabulary)
+    then trains a classifier to predict long or short form of word used
+    :param df: Dataframe
+    :param model: Classifier
+    :return:
 
-    Returns 2d list containing training(70%), validation(15%) and test(15&) data
-    load_data[0] is location of X values (train,val,test)
-    load_data[1] is location of Y values (train,val,test)
-    real_file: str
-        location of real headline file
-    fake_file: str
-        location of fake headline file
     """
     try:
         count = min(df.form.value_counts())
     except ValueError:
-        count =0
-    if count<500:
+        count = 0
+    if count < 500:
         return np.zeros(10), np.zeros(10)
     short = df[df["form"] == 0].sample(count)
     long = df[df["form"] == 1].sample(count)
@@ -33,34 +31,44 @@ def train_model(df):
     vectorizor = CountVectorizer(min_df=0.001)
     X_fitted = vectorizor.fit_transform(headlines)
     X = X_fitted.toarray()
-    lr = LogisticRegression()
 
-    lr_scores = cross_val_score(lr, X, Y, cv=10)
+    lr_scores = cross_val_score(model, X, Y, cv=10)
     return lr_scores.mean()
 
 
-def compute_scores(df):
+def compute_scores(df, model):
+    """
+
+    :param df: Dataframe
+    :param model: Classifier
+    :return:
+    """
     lr_scores = []
 
     for i in range(23):
         if i != 0:
             df1 = df[df["sense"] == i]
         else:
-            df1=df
+            df1 = df
+        if model == "LR":
+            m = LogisticRegression()
+        else:
+            m = RandomForestClassifier(n_estimators=20)
 
-        all_score = train_model(df1)
+        all_score = train_model(df1, m)
         lr_scores.append(all_score)
     return lr_scores
 
 
 if __name__ == "__main__":
+    model = sys.argv[1]
     corpora = ["native", "nonnative", "wiki"]
     all_rf_scores = []
     all_lr_scores = {}
     for corpus in corpora:
         file_in = f"/ais/hal9000/masih/sentences/{corpus}_sentences_False.csv"
         df = pd.read_csv(file_in, encoding="utf-8")
-        s = compute_scores(df)
+        s = compute_scores(df, model)
 
         all_lr_scores[corpus] = s
 

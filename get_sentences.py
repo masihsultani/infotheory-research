2ffd6be6bdea25f9434b438eb3d46f8cbc32c2ba
Masih Sultani <masih@hal9000.ais.sandbox>
2019-12-04 00:17:25 -0500

    fix issue where substring was removed

--- get_sentences.py
+++ get_sentences.py
@@ -41,10 +41,10 @@ def main_prog(infile, out_file,csv_file, stop_words=False):
                 if len(cleaned_words) > 14:
                     for word in cleaned_words:
                         if word in short_forms:
-                            temp = [cleaned_string.replace(word,""), 0, word]
+                            temp = [cleaned_string.replace(f"{word} ",""), 0, word]
                             csvout.writerow(temp)
                         elif word in long_forms:
-                            temp = [cleaned_string.replace(word,""), 1, word]
+                            temp = [cleaned_string.replace(f"{word} ",""), 1, word]
                             csvout.writerow(temp)
                         else:
                             continue
